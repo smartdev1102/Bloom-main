039dc23f8f0cde3262c62304f9588e0f41acdd70
smartdev1102 <hfish7789@gmail.com>
2023-04-01 08:34:42 +0900

    fix: delete unused data
    Co-authored-by: LiuJin1102 liujin941102@gmail.com

--- components/Profile.js
+++ components/Profile.js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { userService } from "services";
